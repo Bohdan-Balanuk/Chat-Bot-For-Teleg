34a452b78c691c9c0e5f3f56fb9eb04a61b10b1d
Bohdan <boganbalanuk88@gmail.com>
2023-05-06 14:48:44 +0300

    modified:   bot.py 	new file:   bot_experement.py

--- bot.py
+++ bot.py
@@ -2,25 +2,39 @@
 BOT_TOKEN = '6055763111:AAFMbPP0-q3w_dA4E-xwf8H2W7hnwYrA3VI'
 
 import telebot 
+from telebot import types # для указание типов
 import requests
  
 # Створюємо обект бот і даємо токен
 bot = telebot.TeleBot(BOT_TOKEN)
 
-def get_locaion_citi(citi):
+markup = types.InlineKeyboardMarkup(row_width=1)
+first = types.InlineKeyboardButton("1", callback_data="1")
+second = types.InlineKeyboardButton("2", callback_data="2")
+third = types.InlineKeyboardButton("3", callback_data="3")
+fourth = types.InlineKeyboardButton("4", callback_data="4")
+fifth = types.InlineKeyboardButton("5", callback_data="5")
+sixth = types.InlineKeyboardButton("6", callback_data="6")
+seventh = types.InlineKeyboardButton("7", callback_data="7")
+eighth = types.InlineKeyboardButton("8", callback_data="8")
+nineth = types.InlineKeyboardButton("9", callback_data="9")
+tenth = types.InlineKeyboardButton("10", callback_data="10")
+markup.add(first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, tenth)
+
+def get_locaion_citi(citi,message):
     location_perms = {
         'name': citi,
     }
-    
+
     response = requests.get('https://geocoding-api.open-meteo.com/v1/search', params=location_perms)
     data = response.json()
     i = 0
-
+    
     for i in range(len(data['results'])):
-        print(data['results'][i]['name'] + ' - ' + str(i+1))
+        bot.send_message(message.chat.id, data['results'][i]['name'] + ' - ' + str(i+1))
 
-    choose = input('\nВиберіть місто, яке вам потрібне, з цих перерахованих ')
-
+    bot.send_message(message.chat.id, 'Виберіть місто, яке вам потрібне, з цих перерахованих', reply_markup = markup)
+    
     location_citit = [
         data['results'][int(choose)-1]['latitude'],
         data['results'][int(choose)-1]['longitude']
@@ -45,21 +59,46 @@ def get_info_about_weather(location):
 
 
 # обробник повідомлень
-@bot.message_handler(commands=['star'])
+@bot.message_handler(commands=['start'])
 def start_function(message):
     # reply_to відповісти на 
-    bot.reply_to(message, "Привіт я бот який показує погоду в певному місті")
+    markup = types.ReplyKeyboardMarkup()
+    start = types.KeyboardButton("СТАРТ")
+    stop = types.KeyboardButton("СТОП")
+    markup.add(start, stop)
+    bot.send_message(message.chat.id, text = "Привіт, {0.first_name}! Я бот що показує погоду, натисник на кнопку для старту".format(message.from_user), reply_markup=markup)
+    #
+
+@bot.callback_query_handler(lambda call: True)
+def handle(call):
+    bot.send_message(chat_id=call.message.chat.id, message_id=call.message.id, text='It works!')
+bot.answer_callback_query(call.id)
 
 # Обробник повідомлень яка відповідає на будь-яке повідомлення
 @bot.message_handler(content_types=['text'])
 def react_on_message(message):
-    if message.text == "повтори":
-        bot.reply_to(message, message.text)
+    if message.text == "СТАРТ":
+        bot.reply_to(message, "Напишіть місто на латині")
+    if message.text == "СТОП":
+        bot.reply_to(message, "Бувай")
+        exit()
+    
+    town = message.text
+    location = get_locaion_citi(town, message)
+    temperatura_citi = get_info_about_weather(location)
 
-    if message.text == "Привіт" or message.text == "привіт":
-        text = "Напишіть місто на латині"
-        sent_msg = bot.send_message(message.chat.id, text, parse_mode='Markdown')
-        bot.register_next_step_handler(sent_msg, weather(text))
+    bot.reply_to(message, "Місце знаходження: " +str(location))
+    bot.reply_to(message, "Температура в місті" + str(temperatura_citi))
+
+
+    
+    # if message.text == "повтори":
+    #     bot.reply_to(message, message.text)
+
+    # if message.text == "Привіт" or message.text == "привіт":
+    #     text = input("Напишіть місто на латині")
+    #     sent_msg = bot.send_message(message.chat.id, text, parse_mode='Markdown')
+    #     bot.register_next_step_handler(sent_msg, weather(text))
 
     # if message.text == "Привіт" or message.text == "привіт":
     #     text = "Привіт як в тебе справи?"
@@ -91,15 +130,6 @@ def mode_react(message):
     if message.text == "я розбив річ":
         bot.reply_to(message, "Я куплю тобі нову!")
 
-@bot.message_handler(commands=['start'])
-def weather(message):
-    # reply_to відповісти на 
-    ask_citi = message
-    location = get_locaion_citi(ask_citi)
-    temperatura_citi = get_info_about_weather(location)
-
-    bot.reply_to("Місце знаходження міста: " + str(location))
-    bot.reply_to("Температура в місті: " + str(temperatura_citi))
 
 bot.infinity_polling() # працювати завжди
 
---
+++ bot_experement.py
@@ -0,0 +1,111 @@
+# Ключ для доступу і керування ботом
+BOT_TOKEN = '6055763111:AAFMbPP0-q3w_dA4E-xwf8H2W7hnwYrA3VI'
+
+import telebot 
+from telebot import types # для указание типов
+import requests
+import sys 
+
+# Створюємо обект бот і даємо токен
+bot = telebot.TeleBot(BOT_TOKEN)
+
+def get_info_about_weather(location):
+    wether_params = {
+        'latitude': location[0],
+        'longitude': location[1],
+        'current_weather': True
+    }
+
+    # запитати і зберегти відповідь у змінну response
+    response = requests.get('https://api.open-meteo.com/v1/forecast', params=wether_params)
+    data = response.json() # зберігається json-відповідь
+
+    tmperatura_citi = data['current_weather']['temperature']
+
+    return tmperatura_citi
+
+# обробник повідомлень
+@bot.message_handler(commands=['start'])
+def start_function(message):
+    # reply_to відповісти на 
+    markup = types.ReplyKeyboardMarkup()
+    start = types.KeyboardButton("СТАРТ")
+    stop = types.KeyboardButton("СТОП")
+    markup.add(start, stop)
+    bot.send_message(message.chat.id, text = "Привіт, {0.first_name}! Я бот що показує погоду, натисник на кнопку для старту".format(message.from_user), reply_markup=markup)
+    #
+
+citi = ''
+retry = 0
+# Обробник повідомлень яка відповідає на будь-яке повідомлення
+@bot.message_handler(content_types=['text'])
+def react_on_message(message):
+    global citi, retry
+    if message.text == "СТАРТ":
+        bot.reply_to(message, "Напишіть місто на латині")
+    elif message.text == "СТОП":
+        bot.reply_to(message, "Бувай")        
+
+    else:
+        if message.text == "1":
+            choose = 1
+            retry = 1
+        elif message.text == "2":
+            choose = 2
+            retry = 1
+        elif message.text == "3":
+            choose = 3
+            retry = 1
+        elif message.text == "4":
+            choose = 4
+            retry = 1
+        elif message.text == "5":
+            choose = 5
+            retry = 1
+        elif message.text == "6":
+            choose = 6
+            retry = 1
+        elif message.text == "7":
+            choose = 7
+            retry = 1
+        elif message.text == "8":
+            choose = 8
+            retry = 1
+        elif message.text == "9":
+            choose = 9
+            retry = 1
+        elif message.text == "10":
+            choose = 10
+            retry = 1
+
+        else:           
+            town = message.text
+            print(town)
+            citi = town
+        
+        location_perms = {
+            'name': citi,
+        }
+
+        response = requests.get('https://geocoding-api.open-meteo.com/v1/search', params=location_perms)
+        data = response.json()
+        i = 0
+
+        if retry == 0:
+            for i in range(len(data['results'])):
+                bot.send_message(message.chat.id, data['results'][i]['name'] + ' - ' + str(i+1))
+
+            bot.send_message(message.chat.id, 'Виберіть місто, яке вам потрібне, з цих перерахованих')#, reply_markup = markup)
+
+        location_citit = [
+                data['results'][int(choose)-1]['latitude'],
+                data['results'][int(choose)-1]['longitude']
+            ]
+
+        temperatura_citi = get_info_about_weather(location_citit)
+
+        bot.reply_to(message, "Місце знаходження: " +str(location_citit))
+        bot.reply_to(message, "Температура в місті" + str(temperatura_citi))
+
+
+bot.infinity_polling() # працювати завжди
